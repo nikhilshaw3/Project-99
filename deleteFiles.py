f7f454154353581e3fc83012b444c6c1fe0a5816
nikhilshaw3 <69413813+nikhilshaw3@users.noreply.github.com>
2021-02-22 10:42:09 +0530

    Add files via upload

---
+++ deleteFiles.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import time
+
+path=input("Enter path of the folder .")
+days=30
+seconds=time.time()-(days*24*60*60)
+
+
+if (os.path.exists(path)):
+    for root,dirs,files in os.walk(path,topdown=True):
+        for name in files:
+            path=os.path.join(root,name)
+            ctime=os.stat(path).st_ctime
+            
+            
+            
+
+            if (seconds>=ctime):
+                os.remove(path)
+                print("\n Deleted the path" + path + "successfully")
+
+                
+
+        for name in dirs:
+            path = os.path.join(root, name)
+            ctime = os.stat(path).st_ctime
+
+            
+
+            if (seconds >= ctime):
+                shutil.rmtree(path)
+                print("\n Deleted the path " + path + " successfully")
+
+
+else:
+    print("\n Path not found")
